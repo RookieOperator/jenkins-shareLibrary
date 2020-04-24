1899aee7b510cf9c159ed97497b8803a2e572e08
cool-ops <34888500+cool-ops@users.noreply.github.com>
2020-04-24 09:32:48 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -3,9 +3,13 @@ package org.devops
 // 构建类型
 
 // 定义一个buildTools的map
-def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+// def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
 
 def Build(buildType,buildShell){
+  
+  // 定义一个buildTools的map
+  def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+  
   println("当前选择的构建类型是${buildType}")
   // 获取buildHome
   buildHome = tool buildTools[buildType]
